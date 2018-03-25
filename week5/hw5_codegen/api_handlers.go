5e820e8f1e91f25c5b9731c1e1d9c5a5cd57d37d
maxtyutmanov <tyutmanovmd@gmail.com>
2018-03-26 00:02:30 +0300

    more work on code generation assignment

--- week5/hw5_codegen/api_handlers.go
+++ week5/hw5_codegen/api_handlers.go
@@ -2,148 +2,257 @@ package main
 
 import "fmt"
 import "net/http"
-import "strings"
 import "strconv"
+import "encoding/json"
 
 
-	type ApiValidator struct {
-	}
-	
-	func (validator *ApiValidator) ValidateRequiredStr(fieldName string, fieldValue string) (*ApiError) {
-		if (fieldValue == "") {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must me not empty", fieldName)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateRequiredInt(fieldName string, fieldValue int) (*ApiError) {
-		if (fieldValue == 0) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must me not empty", fieldName)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMinStr(fieldName string, fieldValue string, min int) (*ApiError) {
-		if (len(fieldValue) < min) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s len must be >= %v", fieldName, min)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMinInt(fieldName string, fieldValue int, min int) (*ApiError) {
-		if (fieldValue < min) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must be >= %v", fieldName, min)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMaxStr(fieldName string, fieldValue string, max int) (*ApiError) {
-		if (len(fieldValue) < max) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s len must be <= %v", fieldName, max)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMaxInt(fieldName string, fieldValue int, max int) (*ApiError) {
-		if (fieldValue < max) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must be <= %v", fieldName, max)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateEnum(fieldName string, 
-		fieldValue string, allowedValues []string, defaultValue string) (*ApiError) {
-	
-		for _, val := range allowedValues {
-			if val == fieldValue {
-				return nil
-			}
-		}
-	
-		err := fmt.Errorf("%s must be one of [%s]", fieldName, strings.Join(allowedValues, ", "))
-		return &ApiError{http.StatusBadRequest, err}
-	}
 
-	func checkAuth(w http.ResponseWriter, r *http.Request) *ApiError {
-		if (r.Header.Get("X-Auth") != "100500") {
-			return &ApiError{http.StatusUnauthorized, fmt.Errorf("unauthorized")}
-		}
-		return nil
+func checkAuth(w http.ResponseWriter, r *http.Request) *ApiError {
+	if (r.Header.Get("X-Auth") != "100500") {
+		return &ApiError{http.StatusUnauthorized, fmt.Errorf("unauthorized")}
 	}
+	return nil
+}
 
-	func handleError(err *ApiError, w http.ResponseWriter) {
-		w.WriteHeader(err.HTTPStatus)
-		w.Write([]byte(err.Err.Error()))
-	}
+func handleError(err *ApiError, w http.ResponseWriter) {
+	w.WriteHeader(err.HTTPStatus)
+	w.Write([]byte(err.Err.Error()))
+}
 
 
-	func (api *MyApi) handlerProfile(w http.ResponseWriter, r *http.Request) {
-		var err *ApiError
-		
-		deserializedParams, err := deserializeProfileParams(r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		result, err := api.Profile(r.Context(), deserializedParams)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		resultStr := json.Marshal(result)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(resultStr)
+func (api *MyApi) handlerProfile(w http.ResponseWriter, r *http.Request) {
+	var err *ApiError
+	
+	
+	deserializedParams, err := deserializeProfileParams(r)
+	if err != nil {
+		handleError(err, w)
+		return
 	}
-	
-	func (api *MyApi) handlerCreate(w http.ResponseWriter, r *http.Request) {
-		var err *ApiError
-		
-		// checking authentication
-		err = checkAuth(w, r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		deserializedParams := deserialize()
-	
-		deserializedParams, err := deserializeCreateParams(r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		result, err := api.Create(r.Context(), deserializedParams)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		resultStr := json.Marshal(result)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(resultStr)
+	result, err := api.Profile(r.Context(), deserializedParams)
+	if err != nil {
+		handleError(err, w)
+		return
 	}
+	resultStr := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resultStr))
+}
+
+func (api *MyApi) handlerCreate(w http.ResponseWriter, r *http.Request) {
+	var err *ApiError
 	
-	func (api *OtherApi) handlerCreate(w http.ResponseWriter, r *http.Request) {
-		var err *ApiError
-		
-		// checking authentication
-		err = checkAuth(w, r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		deserializedParams := deserialize()
-	
-		deserializedParams, err := deserializeOtherCreateParams(r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		result, err := api.Create(r.Context(), deserializedParams)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		resultStr := json.Marshal(result)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(resultStr)
+	// checking http method
+	if r.Method != "POST" {
+		handleError(&ApiError{http.StatusNotAcceptable,fmt.Errorf("bad method")})
+		return
 	}
+
 	
+	// checking authentication
+	err = checkAuth(w, r)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+
+	deserializedParams, err := deserializeCreateParams(r)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	result, err := api.Create(r.Context(), deserializedParams)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	resultStr := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resultStr))
+}
+
+func (api *OtherApi) handlerCreate(w http.ResponseWriter, r *http.Request) {
+	var err *ApiError
+	
+	// checking http method
+	if r.Method != "POST" {
+		handleError(&ApiError{http.StatusNotAcceptable,fmt.Errorf("bad method")})
+		return
+	}
+
+	
+	// checking authentication
+	err = checkAuth(w, r)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+
+	deserializedParams, err := deserializeOtherCreateParams(r)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	result, err := api.Create(r.Context(), deserializedParams)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	resultStr := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resultStr))
+}
+//Deserializer for struct ProfileParams
+
+func deserializeProfileParams(r *http.Request) (ProfileParams, *ApiError) {
+	model := ProfileParams{}
+	
+	var valStr string
+
+	valStr = r.FormValue("login")
+
+	if valStr == "" {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Login must me not empty")}
+	}
+
+	model.Login = valStr
+
+
+	return model, nil
+}
+//Deserializer for struct CreateParams
+
+func deserializeCreateParams(r *http.Request) (CreateParams, *ApiError) {
+	model := CreateParams{}
+	
+	var valStr string
+
+	valStr = r.FormValue("login")
+
+	if valStr == "" {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Login must me not empty")}
+	}
+
+	model.Login = valStr
+
+	if len(model.Login) < 10 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Login len must be >= 10")}
+	}
+
+
+	valStr = r.FormValue("full_name")
+
+	model.Name = valStr
+
+
+	valStr = r.FormValue("status")
+
+	
+	if valStr == "" {
+		valStr = "user"
+	}
+
+	switch (valStr) {
+	case "user", "moderator", "admin":
+		//ok!
+	default:
+		err := fmt.Errorf("Status must be one of [user, moderator, admin]")
+		return nil, &ApiError{http.StatusBadRequest, err}
+	}
+
+	model.Status = valStr
+
+
+	valStr = r.FormValue("age")
+
+	model.Age, _ = strconv.Atoi(valStr)
+
+	if model.Age < 0 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Age must be >= 0")}
+	}
+
+	if model.Age > 128 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Age must be <= 128")}
+	}
+
+
+	return model, nil
+}
+//Deserializer for struct OtherCreateParams
+
+func deserializeOtherCreateParams(r *http.Request) (OtherCreateParams, *ApiError) {
+	model := OtherCreateParams{}
+	
+	var valStr string
+
+	valStr = r.FormValue("username")
+
+	if valStr == "" {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Username must me not empty")}
+	}
+
+	model.Username = valStr
+
+	if len(model.Username) < 3 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Username len must be >= 3")}
+	}
+
+
+	valStr = r.FormValue("account_name")
+
+	model.Name = valStr
+
+
+	valStr = r.FormValue("class")
+
+	
+	if valStr == "" {
+		valStr = "warrior"
+	}
+
+	switch (valStr) {
+	case "warrior", "sorcerer", "rouge":
+		//ok!
+	default:
+		err := fmt.Errorf("Class must be one of [warrior, sorcerer, rouge]")
+		return nil, &ApiError{http.StatusBadRequest, err}
+	}
+
+	model.Class = valStr
+
+
+	valStr = r.FormValue("level")
+
+	model.Level, _ = strconv.Atoi(valStr)
+
+	if model.Level < 1 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Level must be >= 1")}
+	}
+
+	if model.Level > 50 {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("Level must be <= 50")}
+	}
+
+
+	return model, nil
+}
+
+func (h *MyApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/user/profile":
+		h.handlerProfile(w, r)
+	case "/user/create":
+		h.handlerCreate(w, r)
+	default:
+		handleError(&ApiError{http.StatusBadRequest, fmt.Errorf("unknown method")})
+	}
+}
+
+func (h *OtherApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/user/create":
+		h.handlerCreate(w, r)
+	default:
+		handleError(&ApiError{http.StatusBadRequest, fmt.Errorf("unknown method")})
+	}
+}
--- week5/hw5_codegen/handlers_gen/codegen.go
+++ week5/hw5_codegen/handlers_gen/codegen.go
@@ -11,143 +11,88 @@ import (
 	"fmt"
 	"strings"
 	"io"
+	"reflect"
+	"bytes"
 )
 
 type handlerTplArg struct {
 	ApiTypeName string
 	ApiMethodName string
 	CheckAuthBlock string
+	CheckHttpMethodBlock string
 	ParamsTypeName string
+}
+
+type deserializeModelTplArg struct {
+	ModelTypeName string
+	DeserializeFieldsCode string
+}
+
+type handlerConfig struct {
+	ApiMethodName string
+	UrlPath string
 }
 
 var (
 	genPackages = []string{
 		"fmt",
 		"net/http",
-		"strings",
 		"strconv",
+		"encoding/json",
 	}
 
 	helperFuncsSrc = `
-	type ApiValidator struct {
-	}
-	
-	func (validator *ApiValidator) ValidateRequiredStr(fieldName string, fieldValue string) (*ApiError) {
-		if (fieldValue == "") {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must me not empty", fieldName)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateRequiredInt(fieldName string, fieldValue int) (*ApiError) {
-		if (fieldValue == 0) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must me not empty", fieldName)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMinStr(fieldName string, fieldValue string, min int) (*ApiError) {
-		if (len(fieldValue) < min) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s len must be >= %v", fieldName, min)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMinInt(fieldName string, fieldValue int, min int) (*ApiError) {
-		if (fieldValue < min) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must be >= %v", fieldName, min)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMaxStr(fieldName string, fieldValue string, max int) (*ApiError) {
-		if (len(fieldValue) < max) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s len must be <= %v", fieldName, max)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateMaxInt(fieldName string, fieldValue int, max int) (*ApiError) {
-		if (fieldValue < max) {
-			return &ApiError{http.StatusBadRequest, fmt.Errorf("%s must be <= %v", fieldName, max)}
-		}
-		return nil
-	}
-	
-	func (validator *ApiValidator) ValidateEnum(fieldName string, 
-		fieldValue string, allowedValues []string, defaultValue string) (*ApiError) {
-	
-		for _, val := range allowedValues {
-			if val == fieldValue {
-				return nil
-			}
-		}
-	
-		err := fmt.Errorf("%s must be one of [%s]", fieldName, strings.Join(allowedValues, ", "))
-		return &ApiError{http.StatusBadRequest, err}
-	}
 
-	func checkAuth(w http.ResponseWriter, r *http.Request) *ApiError {
-		if (r.Header.Get("X-Auth") != "100500") {
-			return &ApiError{http.StatusUnauthorized, fmt.Errorf("unauthorized")}
-		}
-		return nil
+func checkAuth(w http.ResponseWriter, r *http.Request) *ApiError {
+	if (r.Header.Get("X-Auth") != "100500") {
+		return &ApiError{http.StatusUnauthorized, fmt.Errorf("unauthorized")}
 	}
+	return nil
+}
 
-	func handleError(err *ApiError, w http.ResponseWriter) {
-		w.WriteHeader(err.HTTPStatus)
-		w.Write([]byte(err.Err.Error()))
+func handleError(err *ApiError, w http.ResponseWriter) {
+	w.WriteHeader(err.HTTPStatus)
+	w.Write([]byte(err.Err.Error()))
+}
+`
+
+	handlerTpl = template.Must(template.New("handler").Parse(`
+func (api *{{.ApiTypeName}}) handler{{.ApiMethodName}}(w http.ResponseWriter, r *http.Request) {
+	var err *ApiError
+	{{.CheckHttpMethodBlock}}
+	{{.CheckAuthBlock}}
+	deserializedParams, err := deserialize{{.ParamsTypeName}}(r)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	result, err := api.{{.ApiMethodName}}(r.Context(), deserializedParams)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	resultStr := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resultStr))
+}
+`))
+
+	checkAuthenticationStr = `
+	// checking authentication
+	err = checkAuth(w, r)
+	if err != nil {
+		handleError(err, w)
+		return
 	}
 `
 
-	checkRequiredIntTpl = template.Must(template.New("checkRequired_int").Parse(`
-		
-	`))
-
-	parseAndValidateIntTpl = template.Must(template.New("parseAndValidate_int").Parse(`
-		// {{.FieldName}}
-		var {{.FieldName}}Raw string
-		{{.FieldName}}Raw = {{.HttpRequestVar}}.FormValue("{{.WhereFrom}}")
-		// Check required validator
-		{{.CheckRequired}}
-		// Substitute empty value with default if the default value is set
-		{{.SubstituteDefaultValue}}
-		// Check other validators
-		{{.CheckValidators}}
-		
-		parsed.{{.FieldName}}, convErr = strconv.Atoi({{.FieldName}}Raw)
-	)
-	`))
-
-	handlerTpl = template.Must(template.New("handler").Parse(`
-	func (api *{{.ApiTypeName}}) handler{{.ApiMethodName}}(w http.ResponseWriter, r *http.Request) {
-		var err *ApiError
-		{{.CheckAuthBlock}}
-		deserializedParams, err := deserialize{{.ParamsTypeName}}(r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		result, err := api.{{.ApiMethodName}}(r.Context(), deserializedParams)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		resultStr := json.Marshal(result)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(resultStr)
-	}
-	`))
-
-	checkAuthenticationStr = `
-		// checking authentication
-		err = checkAuth(w, r)
-		if err != nil {
-			handleError(err, w)
-			return
-		}
-		deserializedParams := deserialize()
-	`
+	deserializeModelTpl = template.Must(template.New("deserializeModel").Parse(`
+func deserialize{{.ModelTypeName}}(r *http.Request) ({{.ModelTypeName}}, *ApiError) {
+	model := {{.ModelTypeName}}{}
+	{{.DeserializeFieldsCode}}
+	return model, nil
+}
+`))
 )
 
 //{"url": "/user/profile", "auth": false}
@@ -155,6 +100,105 @@ var (
 type apigenConfig struct {
 	URL string `json:"url"`
 	Auth bool `json:"auth"`
+	Method string `json:"method"`
+}
+
+func getApiMethodInfo(fDecl *ast.FuncDecl) (apiTypeName string, methodName string, paramsTypeName string) {
+	apiTypeName = fDecl.Recv.List[0].Type.(*ast.StarExpr).X.(*ast.Ident).Name
+	methodName = fDecl.Name.Name
+	paramsTypeName = fDecl.Type.Params.List[1].Type.(*ast.Ident).Name
+	return
+}
+
+func generateHandler(out io.Writer, fDecl *ast.FuncDecl, config apigenConfig) (
+	apiTypeName string, apiMethodName string, paramsTypeName string) {
+
+	apiTypeName, apiMethodName, paramsTypeName = getApiMethodInfo(fDecl)
+
+	fmt.Printf("Generating handler for %s.%s(%s)\n", apiTypeName, apiMethodName, paramsTypeName)
+
+	authCheck := ""
+	if config.Auth {
+		authCheck = checkAuthenticationStr
+	}
+	httpMethodCheck := ""
+	if config.Method != "" {
+		httpMethodCheck = `
+	// checking http method
+	if r.Method != "` + config.Method + `" {
+		handleError(&ApiError{http.StatusNotAcceptable,fmt.Errorf("bad method")})
+		return
+	}
+`
+	}
+
+	handlerTpl.Execute(out, handlerTplArg{
+		ApiMethodName: apiMethodName,
+		ApiTypeName: apiTypeName,
+		CheckAuthBlock: authCheck,
+		CheckHttpMethodBlock: httpMethodCheck,
+		ParamsTypeName: paramsTypeName,
+	})
+
+	return apiTypeName, apiMethodName, paramsTypeName
+}
+
+func generateDeserializers(out io.Writer, node *ast.File, paramsTypesNames map[string]bool) {
+	for _, f := range node.Decls {
+		g, ok := f.(*ast.GenDecl)
+		if !ok {
+			fmt.Printf("SKIP %T is not *ast.GenDecl\n", f)
+			continue
+		}
+		for _, spec := range g.Specs {
+			currType, ok := spec.(*ast.TypeSpec)
+			if !ok {
+				fmt.Printf("SKIP %T is not ast.TypeSpec\n", spec)
+				continue
+			}
+
+			currTypeName := currType.Name.Name
+
+			if !paramsTypesNames[currTypeName] {
+				fmt.Printf("SKIP %s is not a type of handler's parameter\n", currTypeName)
+				continue;
+			}
+
+			currStruct, ok := currType.Type.(*ast.StructType)
+			if !ok {
+				fmt.Printf("SKIP %T is not ast.StructType\n", currStruct)
+				continue
+			}
+
+			generateDeserializerForStruct(out, currStruct, currTypeName)
+		}
+	}
+}
+
+func generateDeserializerForStruct(out io.Writer, currStruct *ast.StructType, structName string) {
+	fmt.Fprintf(out, "//Deserializer for struct %s\n", structName)
+
+	var fieldsBuf bytes.Buffer
+	fieldsBuf.WriteString(`
+	var valStr string
+`)
+	for _, field := range currStruct.Fields.List {
+		if field.Tag != nil {
+			fieldName := field.Names[0].Name
+			fieldType := field.Type.(*ast.Ident).Name
+			tag := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1])
+			fieldMetaStr := tag.Get("apivalidator")
+
+			fmt.Printf("Generating deserialization and validation for field %s with tag %s\n", fieldName, fieldMetaStr)
+
+			fieldsBuf.WriteString(generateFieldDeserializer(fieldName, fieldType, fieldMetaStr))
+			fieldsBuf.WriteRune('\n')
+		}
+	}
+	deserializeModelTpl.Execute(out, deserializeModelTplArg{
+		ModelTypeName: structName,
+		DeserializeFieldsCode: fieldsBuf.String(),
+	})
 }
 
 /*
@@ -166,42 +210,145 @@ type OtherCreateParams struct {
 }
 */
 
-func generateDeserializer(out io.Writer, paramType *ast.StructType) {
-	// for _, field := range paramType.Fields.List {
-	// 	fieldName := field.Names[0].Name
+func generateFieldDeserializer(fieldName string, fieldType string, fieldMetaStr string) (string) {
+	var fieldBuf bytes.Buffer
 
-	// 	if field.Tag != nil {
-	// 		tag := reflect.StructTag(field.Tag.Value)
-	// 		if tag.Get("apivalidator") == "-" {
-				
-	// 		}
-	// 	}
-	// }
-}
+	var whereFrom string
+	tags := make(map[string]string)
 
-func getApiMethodInfo(fDecl *ast.FuncDecl) (apiTypeName string, methodName string, paramsTypeName string) {
-	apiTypeName = fDecl.Recv.List[0].Type.(*ast.StarExpr).X.(*ast.Ident).Name
-	methodName = fDecl.Name.Name
-	paramsTypeName = fDecl.Type.Params.List[1].Type.(*ast.Ident).Name
-	return
-}
-
-func generateHandler(out io.Writer, fDecl *ast.FuncDecl, config apigenConfig) {
-	apiTypeName, apiMethodName, paramsTypeName := getApiMethodInfo(fDecl)
-
-	fmt.Printf("Generating handler for %s.%s(%s)\n", apiTypeName, apiMethodName, paramsTypeName)
-
-	authCheck := ""
-	if config.Auth {
-		authCheck = checkAuthenticationStr
+	if fieldMetaStr != "" {
+		tagsArr := strings.Split(fieldMetaStr, ",")
+		for _, tag := range tagsArr {
+			tagSplit := strings.Split(tag, "=")
+			if len(tagSplit) == 2 {
+				tags[tagSplit[0]] = tagSplit[1]
+			} else if len(tagSplit) == 1 {
+				tags[tagSplit[0]] = "true"
+			} else {
+				panic("Unsupported field tag")
+			}
+		}
+	} else {
+		tags = make(map[string]string)
 	}
 
-	handlerTpl.Execute(out, handlerTplArg{
-		ApiMethodName: apiMethodName,
-		ApiTypeName: apiTypeName,
-		CheckAuthBlock: authCheck,
-		ParamsTypeName: paramsTypeName,
-	})
+	paramnameTag, isSet := tags["paramname"]
+	if isSet {
+		whereFrom = paramnameTag
+	} else {
+		whereFrom = strings.ToLower(fieldName)
+	}
+
+	_, isRequired := tags["required"]
+
+	defaultVal, defaultValIsSet := tags["default"]
+	
+	fieldBuf.WriteString(`
+	valStr = r.FormValue("` + whereFrom +`")
+`)
+
+	if defaultValIsSet {
+		fieldBuf.WriteString(`
+	
+	if valStr == "" {
+		valStr = "` + defaultVal + `"
+	}
+`)
+	}
+
+	if isRequired {
+		fieldBuf.WriteString(`
+	if valStr == "" {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("` + fieldName + ` must me not empty")}
+	}
+`)
+	}
+
+	enumVals, enumValsAreSet := tags["enum"]
+	if enumValsAreSet {
+		split := strings.Split(enumVals, "|")
+		commaSeparated := strings.Join(split, ", ")
+		splitWithQuotes := make([]string, 0, len(split))
+
+		for _, s := range split {
+			splitWithQuotes = append(splitWithQuotes, "\"" + s + "\"")
+		}
+		commaSeparatedWithQuotes := strings.Join(splitWithQuotes, ", ")
+
+		fieldBuf.WriteString(`
+	switch (valStr) {
+	case ` + commaSeparatedWithQuotes + `:
+		//ok!
+	default:
+		err := fmt.Errorf("` + fieldName + ` must be one of [` + commaSeparated + `]")
+		return nil, &ApiError{http.StatusBadRequest, err}
+	}
+`)
+	}
+
+	if (fieldType == "string") {
+		fieldBuf.WriteString(`
+	model.` + fieldName + ` = valStr
+`)
+	} else if (fieldType == "int") {
+		fieldBuf.WriteString(`
+	model.` + fieldName + `, _ = strconv.Atoi(valStr)
+`)
+	}
+
+	minVal, minValIsSet := tags["min"]
+	if minValIsSet && fieldType == "string" {
+		fieldBuf.WriteString(`
+	if len(model.` + fieldName + `) < ` + minVal + ` {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("` + fieldName + ` len must be >= ` + minVal + `")}
+	}
+`)
+	}
+	if minValIsSet && fieldType == "int" {
+		fieldBuf.WriteString(`
+	if model.` + fieldName + ` < ` + minVal + ` {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("` + fieldName + ` must be >= ` + minVal + `")}
+	}
+`)
+	}
+
+	maxVal, maxValIsSet := tags["max"]
+	if maxValIsSet && fieldType == "string" {
+		fieldBuf.WriteString(`
+	if len(model.` + fieldName + `) > ` + maxVal + ` {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("` + fieldName + ` len must be <= ` + maxVal + `")}
+	}
+`)
+	}
+	if maxValIsSet && fieldType == "int" {
+		fieldBuf.WriteString(`
+	if model.` + fieldName + ` > ` + maxVal + ` {
+		return nil, &ApiError{http.StatusBadRequest, fmt.Errorf("` + fieldName + ` must be <= ` + maxVal + `")}
+	}
+`)
+	}
+
+	return fieldBuf.String()
+}
+
+func generateServeHttp(out io.Writer, apiTypeName string, configs []handlerConfig) {
+	fmt.Fprint(out, `
+func (h *` + apiTypeName + `) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {`)
+	for _, config := range configs {
+		fmt.Fprint(out, `
+	case "` + config.UrlPath + `":
+		h.handler` + config.ApiMethodName + `(w, r)`)
+	}
+
+	fmt.Fprint(out, `
+	default:
+		handleError(&ApiError{http.StatusBadRequest, fmt.Errorf("unknown method")})`)
+
+	fmt.Fprint(out, `
+	}
+}
+`)
 }
 
 func main() {
@@ -221,6 +368,10 @@ func main() {
 	fmt.Fprintln(out) // empty line
 	fmt.Fprintln(out, helperFuncsSrc)
 
+	paramsTypesNames := make(map[string]bool)
+	handlerConfigs := make(map[string][]handlerConfig)
+
+	// generate entry-point handlers
 	for _, f := range node.Decls {
 		fDecl, isFunc := f.(*ast.FuncDecl); 
 		if !isFunc {
@@ -240,6 +391,19 @@ func main() {
 			json.Unmarshal([]byte(configStr), &config)
 		}
 
-		generateHandler(out, fDecl, config)
+		if needCodegen {
+			apiTypeName, apiMethodName, pTypeName := generateHandler(out, fDecl, config)
+			paramsTypesNames[pTypeName] = true		
+			handlerConfigs[apiTypeName] = append(handlerConfigs[apiTypeName], handlerConfig{
+				ApiMethodName: apiMethodName,
+				UrlPath: config.URL,
+			})
+		}
+	}
+
+	generateDeserializers(out, node, paramsTypesNames)
+
+	for apiTypeName, apiHandlerConfigs := range handlerConfigs {
+		generateServeHttp(out, apiTypeName, apiHandlerConfigs)
 	}
 }
